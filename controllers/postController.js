10682aa6c0d64c220e080757e0276963b309be48
sontrinh16 <trinhleson2000@gmail.com>
2020-12-22 17:08:21 +0700

    add get user info

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,6 +7,8 @@ const roomController = require('./../controllers/roomController');
 
 exports.getPosts = catchAsync(async (req, res, next) => {
     const posts = await Post.find({}).populate('author').populate('rooms');
+    //posts = posts.filter(post => { post.authenticate == true});
+    posts = posts.map(post => {delete post.authenticate});
     res.status(200).json({
         status: "success",
         data: {
@@ -17,6 +19,8 @@ exports.getPosts = catchAsync(async (req, res, next) => {
 
 exports.getUserPost = catchAsync(async (req, res, next) => {
     const posts = await Post.find({author: req.user._id}).populate('author').populate('rooms');
+    //posts = posts.filter(post => { post.authenticate == true});
+    posts = posts.map(post => {delete post.authenticate});
     res.status(200).json({
         status: "success",
         data: {
@@ -27,7 +31,7 @@ exports.getUserPost = catchAsync(async (req, res, next) => {
 
 exports.getPost = catchAsync(async (req, res, next) => {
     const post = await Post.findOne({_id: req.params.id}).populate('author').populate('rooms');
-
+    delete post.authenticate;
     res.status(200).json({
         status: "success",
         data: {
