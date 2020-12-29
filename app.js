709187dc192feb9d684dc4c59adf7e98b8594a80
sontrinh16 <trinhleson2000@gmail.com>
2020-12-30 05:36:57 +0700

    remove fav

--- app.js
+++ app.js
@@ -35,8 +35,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+        origin: "*"
       }
 });
 
