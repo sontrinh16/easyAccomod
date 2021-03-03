53938102d76600f3eaa067cdbe2223a12ef99b45
sontrinh16 <trinhleson2000@gmail.com>
2021-03-03 20:53:24 +0700

    add pusher

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -4,15 +4,41 @@ const Review = require('./../models/review');
 const Notification = require('./../models/notification');
 
 exports.getNotifications = catchAsync(async(req, res, next) => {
-    let noti = await Notification.find({});
-    let not_seen_noti = await Notification.find({seen: false});
-    res.status(200).json({
-        status: "success",
-        data: {
-            noti,
-            not_seen_noti: not_seen_noti.length
-        }
-    })
+    if (req.query.limit||req.query.page){
+        const options = {
+            page: req.query.page,
+            limit: req.query.limit,
+            sort: {created : -1},
+            select: {
+                _id: 0,
+                __v: 0
+            }
+        };
+        let docs = await Notification.paginate({}, options);
+        let notifications = docs.docs;
+        let not_seen_noti = await Notification.find({seen: false});
+        res.status(200).json({
+            status: "success",
+            data: {
+                notifications,
+                not_seen_noti: not_seen_noti.length
+            }
+        });
+    }
+    else{
+        let noti = await Notification.find({}).sort('-created').select({
+            _id: 0,
+            __v: 0
+        });
+        let not_seen_noti = await Notification.find({seen: false});
+        res.status(200).json({
+            status: "success",
+            data: {
+                noti,
+                not_seen_noti: not_seen_noti.length
+            }
+        })
+    }
 });
 
 exports.seenNotification = catchAsync(async(req, res, next) => {
